8ce86359f8e27cb12205d3f35028ae0a15bd26df
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-07-28 02:14:02 -0300

    Added player movement

---
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Rotate(0.0f, horizontal * Time.deltaTime * rotationSpeed, 0.0f);
+        transform.Translate(0, 0, vertical * Time.deltaTime * speed);
+    }
+}
