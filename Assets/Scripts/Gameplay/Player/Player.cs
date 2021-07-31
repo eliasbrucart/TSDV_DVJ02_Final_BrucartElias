46de0bdb0b51890c7ea934bb6a1764269e4dded9
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-07-31 02:31:48 -0300

    Solved tower rotation

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -9,8 +9,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float towerSpeed;
     [SerializeField] private Camera cam;
     [SerializeField] private float distanceCamRay;
+    [SerializeField] private GameObject crossfire;
+    [SerializeField] private float crossfireOffset;
 
     private Vector3 mousePos;
+    private float maxTimeAnim = 0.5f;
+    private float timerAnim;
     void Start()
     {
         cam = Camera.main;
@@ -18,8 +22,12 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        MovePlayer();
         Shoot();
+    }
+
+    private void LateUpdate()
+    {
+        MovePlayer();
     }
 
     private void MovePlayer()
@@ -36,6 +44,7 @@ public class Player : MonoBehaviour
         mousePos = Input.mousePosition;
         Ray ray = cam.ScreenPointToRay(mousePos);
         Debug.DrawRay(ray.origin, ray.direction * distanceCamRay, Color.yellow);
+        //crossfire.transform.position = ray.origin + (ray.direction * (distanceCamRay - crossfireOffset));
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -49,8 +58,15 @@ public class Player : MonoBehaviour
 
     IEnumerator RotateTower(RaycastHit hit)
     {
-        yield return new WaitForSeconds(2);
-        Debug.Log("Funca esto");
-        tower.LookAt(hit.point, Vector3.up);
+        while(timerAnim < maxTimeAnim)
+        {
+            timerAnim += Time.deltaTime;
+            Quaternion targetRotation = Quaternion.identity;
+            Vector3 targetDirection = (hit.point - tower.position).normalized;
+            targetRotation = Quaternion.LookRotation(targetDirection);
+            tower.rotation = Quaternion.SlerpUnclamped(tower.rotation, targetRotation, towerSpeed);
+            yield return null;
+        }
+        timerAnim = 0;
     }
 }
