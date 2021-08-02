da0fe20ad630cc2c426f782f9a77af83f41cd70a
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-02 01:43:32 -0300

    Added new camera behavior

---
+++ Assets/Scripts/Gameplay/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private Vector3 offset;
+    void Start()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+
+    private void LateUpdate()
+    {
+        float distance = Vector3.Distance(player.transform.position + offset, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position + offset, (cameraSpeed * distance) * Time.deltaTime);
+    }
+}
