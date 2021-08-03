c8c775cdf0177824d174d02ed945b43d1188cd7a
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 18:15:32 -0300

    improved GameOver scene

---
+++ Assets/Scripts/Game/UI/UIGameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIGameOver : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    public TMP_Text destroyedBoxesText;
+    public TMP_Text distanceText;
+
+    private GameManager gm;
+    void Start()
+    {
+        gm = GameManager.instanceGameManager;
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + gm.score;
+        destroyedBoxesText.text = "Destroyed Boxes: " + gm.destroyedBoxes;
+        distanceText.text = "Tank distance traveled: " + gm.distance;
+    }
+}
