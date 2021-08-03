662051c1e7193354599da6eca1008fff21b7e74d
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 02:47:53 -0300

    Points are now added when breaking boxes

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public float time;
+    public int destroyedBoxes;
+    public float distance;
+
+    [SerializeField] private int scorePerBox;
+
+
+    void Start()
+    {
+        Bomb.CollisionWithBox += AddScore;
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        OutOfTime();
+    }
+
+    private void AddScore()
+    {
+        score += scorePerBox;
+    }
+
+    private void OutOfTime()
+    {
+        if (time <= 0.0f)
+            time = 0.0f;
+    }
+
+    private void OnDisable()
+    {
+        Bomb.CollisionWithBox -= AddScore;
+    }
+}
--- Assets/Scripts/Gameplay/bomb/Bomb.cs
+++ Assets/Scripts/Gameplay/bomb/Bomb.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
+using System;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float despawnTime;
     private Rigidbody rb;
+
+    public static event Action CollisionWithBox;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * speed);
         Destroy(gameObject, despawnTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "box")
+        {
+            Destroy(collision.gameObject);
+            CollisionWithBox?.Invoke();
+            Destroy(gameObject);
+        }
     }
 }
