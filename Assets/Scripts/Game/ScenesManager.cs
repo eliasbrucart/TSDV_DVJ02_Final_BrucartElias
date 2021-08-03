8b34df34eb106f17c62aa6e29cf6e8c333c9b517
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 02:50:36 -0300

    Added ScenesManager

---
+++ Assets/Scripts/Game/ScenesManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    static public ScenesManager instanceScenesManager;
+
+    static public ScenesManager Instance { get { return instanceScenesManager; } }
+
+    private void Awake()
+    {
+        if (instanceScenesManager != this && instanceScenesManager != null)
+            Destroy(this.gameObject);
+        else
+            instanceScenesManager = this;
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ChangeSceneAdditive(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+    }
+
+    public void UnloadSceneAsy(string scene)
+    {
+        SceneManager.UnloadSceneAsync(scene);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
