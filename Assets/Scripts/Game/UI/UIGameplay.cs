44f5b5cd45ff17eac70e13fccb78f4644a62bc06
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 14:56:41 -0300

    Pause Btn is now works

---
+++ Assets/Scripts/Game/UI/UIGameplay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class UIGameplay : MonoBehaviour
+{
+    private GameManager gm;
+    private ScenesManager sc;
+    void Start()
+    {
+        gm = GameManager.instanceGameManager;
+        sc = ScenesManager.instanceScenesManager;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        if(Time.timeScale == 1)
+        {
+            sc.ChangeSceneAdditive("Pause");
+            Time.timeScale = 0;
+        }
+    }
+}
