6f24dd7a33baf7dac3781b62a4d84f239d363a67
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 15:01:17 -0300

    Pause scene now works

---
+++ Assets/Scripts/Game/UI/UIPause.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class UIPause : MonoBehaviour
+{
+    private ScenesManager sc;
+    void Start()
+    {
+        sc = ScenesManager.instanceScenesManager;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        sc.UnloadSceneAsy("Pause");
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        sc.ChangeScene("Menu");
+    }
+}
