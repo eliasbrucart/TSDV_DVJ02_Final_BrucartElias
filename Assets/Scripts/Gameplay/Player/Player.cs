9576c48d9ef640aa019f2899235b7b0eef6b0b35
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-03 01:13:10 -0300

    first version of shoot

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Player : MonoBehaviour
@@ -9,8 +10,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float towerSpeed;
     [SerializeField] private Camera cam;
     [SerializeField] private float distanceCamRay;
-    [SerializeField] private GameObject crossfire;
-    [SerializeField] private float crossfireOffset;
+    [SerializeField] private GameObject bombPrefab;
+    [SerializeField] private Vector3 bombOffset;
 
     private Vector3 mousePos;
     private float maxTimeAnim = 0.5f;
@@ -18,24 +19,19 @@ public class Player : MonoBehaviour
 
     private BoxCollider colliderTank;
     private float halfHeight;
-    private Transform chasis;
+
+    public static event Action ShootEvent;
     void Start()
     {
         cam = Camera.main;
         colliderTank = GetComponent<BoxCollider>();
         halfHeight = colliderTank.bounds.extents.y;
-        chasis = GetComponent<Transform>();
     }
 
     void Update()
     {
         MovePlayer();
         Shoot();
-    }
-
-    private void LateUpdate()
-    {
-        
     }
 
     private void MovePlayer()
@@ -61,7 +57,6 @@ public class Player : MonoBehaviour
         mousePos = Input.mousePosition;
         Ray ray = cam.ScreenPointToRay(mousePos);
         Debug.DrawRay(ray.origin, ray.direction * distanceCamRay, Color.yellow);
-        //crossfire.transform.position = ray.origin + (ray.direction * (distanceCamRay - crossfireOffset));
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -69,19 +64,27 @@ public class Player : MonoBehaviour
             {
                 Debug.Log("Dio click en: " + hit.point);
                 StartCoroutine(RotateTower(hit));
+                //bombPrefab.transform.rotation = Quaternion.Euler(0.0f, 0.0f, tower.rotation.z);
+                Instantiate(bombPrefab, tower.position, new Quaternion(0.0f, 0.0f, tower.rotation.z, tower.rotation.w));
+                ShootEvent?.Invoke();
             }
         }
     }
 
     IEnumerator RotateTower(RaycastHit hit)
     {
-        while(timerAnim < maxTimeAnim)
+        while (timerAnim < maxTimeAnim)
         {
             timerAnim += Time.deltaTime;
             Quaternion targetRotation = Quaternion.identity;
             Vector3 targetDirection = (hit.point - tower.position).normalized;
             targetRotation = Quaternion.LookRotation(targetDirection);
             tower.rotation = Quaternion.SlerpUnclamped(tower.rotation, targetRotation, towerSpeed);
+            //Instantiate(bombPrefab, tower.position, tower.rotation);
+            Quaternion targetRotationBomb = Quaternion.identity;
+            Vector3 targetDirectionBomb = (hit.point - tower.position).normalized;
+            targetRotationBomb = Quaternion.LookRotation(targetDirectionBomb);
+            bombPrefab.transform.rotation = Quaternion.SlerpUnclamped(bombPrefab.transform.rotation, targetRotationBomb, towerSpeed);
             yield return null;
         }
         timerAnim = 0;
---
+++ Assets/Scripts/Gameplay/bomb/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float despawnTime;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Player.ShootEvent += MoveBomb;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void MoveBomb()
+    {
+        rb.AddForce(transform.forward * speed);
+        DestroyBomb();
+    }
+
+    private void DestroyBomb()
+    {
+        Destroy(gameObject, despawnTime);
+    }
+
+    private void OnDisable()
+    {
+        Player.ShootEvent -= MoveBomb;
+    }
+}
