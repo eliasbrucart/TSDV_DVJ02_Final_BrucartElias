2a63c25eae64457bd3aeb53381eb5a927457d35e
eliasbrucart <eliasbrucart@hardgames.com.ar>
2021-08-02 21:49:31 -0300

    Solved tank movement

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -15,19 +15,27 @@ public class Player : MonoBehaviour
     private Vector3 mousePos;
     private float maxTimeAnim = 0.5f;
     private float timerAnim;
+
+    private BoxCollider colliderTank;
+    private float halfHeight;
+    private Transform chasis;
     void Start()
     {
         cam = Camera.main;
+        colliderTank = GetComponent<BoxCollider>();
+        halfHeight = colliderTank.bounds.extents.y;
+        chasis = GetComponent<Transform>();
     }
 
     void Update()
     {
+        MovePlayer();
         Shoot();
     }
 
     private void LateUpdate()
     {
-        MovePlayer();
+        
     }
 
     private void MovePlayer()
@@ -36,7 +44,16 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         transform.Rotate(0.0f, horizontal * Time.deltaTime * rotationSpeed, 0.0f);
+        //chasis.rotation = Quaternion.LookRotation(Vector3.right);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
         transform.Translate(0.0f, 0.0f, vertical * Time.deltaTime * speed);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, -transform.up, out hitInfo))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, hitInfo.normal) * transform.rotation, 0.15f);
+            transform.position = hitInfo.point + hitInfo.normal * halfHeight;
+        }
     }
 
     void Shoot()
